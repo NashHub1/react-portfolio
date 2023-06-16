031bc5c08e8efe3cf5a79c4db9aa20ee4ab1b3e6
Michael Stephens <stephensnash@gmx.de>
2023-06-16 16:29:26 +0200

    Ordner und Farben initialisert

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from './Components/Header/Header'
+import Nav from "./Components/Navigation/Nav";
+import About from "./Components/About/About";
+import Experience from "./Components/Experience/Experience";
+import Services from "./Components/Services/Services";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+function App() {
+  return (
+    <>
+    <Header></Header>
+    <Nav></Nav>
+    <About></About>
+    <Experience></Experience>
+    <Services></Services>
+    <Portfolio></Portfolio>
+    <Testimonials></Testimonials>
+    <Contact></Contact>
+    <Footer></Footer>
+    
+    </>
+  );
+}
+
+export default App;
