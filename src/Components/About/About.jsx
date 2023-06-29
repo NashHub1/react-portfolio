6976b879cc5728e58a0b0981f609e7578df0fdc5
Michael Stephens <stephensnash@gmx.de>
2023-06-29 17:04:26 +0200

    About Angefangen

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import Michael from '../../Assets/MeAndEinstein.png'
 
 const About = () => {
   return (
-    <section id='About'>
-      About
-    </section>
-  )
-}
+    <section id="About">
+      <h5>Good to know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={Michael}  alt="Michael Stephens" />
+          </div>
+        </div>
+        <div className="about_content">
+        <div className="about_cards">
+        <article className="about_card">
+
+        </article>
+        </div>
+
+        </div>
+
+
+      </div>
+    </section>
+  );
+};
+
+export default About;
