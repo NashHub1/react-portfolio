5eda284829adf6c1bf003926a9c63926031f0df0
Michael Stephens <stephensnash@gmx.de>
2023-07-03 19:43:46 +0200

    About fast Fertig

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Michael from '../../Assets/MeAndEinstein.png'
 import { BsFileEarmarkCode } from 'react-icons/bs'
 import { RiTeamFill } from 'react-icons/ri'
+import "./About.css"
 
 
 const About = () => {
@@ -19,20 +20,20 @@ const About = () => {
         <div className="about_content">
           <div className="about_cards">
             <article className="about_card">
-              <BsFileEarmarkCode  className="about_icon"/>
+              <BsFileEarmarkCode className="about_icon" />
               <h5>Erfahrung</h5>
               <small>1,5 Jahre am programmieren</small>
             </article>
             <article className="about_card">
-              <RiTeamFill  className="about_icon"/>
-              <h5>Erfahrung</h5>
-              <small>1,5 Jahre am programmieren</small>
+              <RiTeamFill className="about_icon" />
+              <h5>Projekte</h5>
+              <small>+10 Projekte in verschieden Sprachen</small>
             </article>
           </div>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, facere aspernatur voluptates quaerat, 
-            tempora nisi quisquam maiores exercitationem atque distinctio excepturi cupiditate? Perferendis, 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, facere aspernatur voluptates quaerat,
+            tempora nisi quisquam maiores exercitationem atque distinctio excepturi cupiditate? Perferendis,
             fugiat cupiditate autem aperiam nesciunt cum delectus?
           </p>
           <a href="#Contact" className="btn btn_primary">Let's Talk</a>
