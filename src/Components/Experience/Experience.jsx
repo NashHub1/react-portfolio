f846565203f1703d5c4b250fda730314cc890643
Michael Stephens <stephensnash@gmx.de>
2023-07-04 19:11:28 +0200

    Experience fast fertig

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,11 +1,90 @@
-import React from 'react'
-
+import React from "react";
+import { BsPatchCheckFill } from "react-icons/bs";
+import "./Experience.css";
 const Experience = () => {
   return (
-    <section id='Expercience'>
-      Expercience
-    </section>
-  )
-}
+    <section id="Expercience">
+      <h5 className="text-light">Meine Fähigkeiten</h5>
+      <h2 className="text-light">Abgeschloßene Projekte</h2>
 
-export default Experience
+      <div className="container experience_container">
+        <div className="experience_frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className="experience_details" title="Etwas">
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h5>JavaScript/TypeScript</h5>
+                <progress value="30" max="100"></progress>
+              </div>
+            </article>
+            <article className="experience_details" title="Etwas">
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h5>React</h5>
+                <progress value="30" max="100"></progress>
+              </div>
+            </article>
+            <article className="experience_details" title="Etwas">
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h5>Angular</h5>
+                <progress value="30" max="100"></progress>
+              </div>
+            </article>
+            <article className="experience_details" title="Etwas">
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h5>JavaFX</h5>
+                <progress value="30" max="100"></progress>
+              </div>
+            </article>
+            <article className="experience_details" title="Etwas">
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h5>HTML5 and CSS</h5>
+                <progress value="30" max="100"></progress>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* End of Frontend  */}
+        <div className="experience_backend">
+          <h3>Backend Development</h3>
+          <div className="experience_content">
+            <article className="experience_details" title="Etwas">
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h5>HTML5</h5>
+                <progress value="30" max="100"></progress>
+              </div>
+            </article>
+            <article className="experience_details" title="Etwas">
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h5>JavaFX</h5>
+                <progress value="30" max="100"></progress>
+              </div>
+            </article>
+            <article className="experience_details" title="Etwas">
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h5>JavaFX</h5>
+                <progress value="30" max="100"></progress>
+              </div>
+            </article>
+            <article className="experience_details" title="Etwas">
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h5>JavaFX</h5>
+                <progress value="30" max="100"></progress>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
--- src/Components/Header/HeaderSocials.jsx
+++ src/Components/Header/HeaderSocials.jsx
@@ -6,7 +6,6 @@ function HeaderSocials() {
     <div className='header_socials'>
       <a href='https://www.linkedin.com/in/michael-stephens-7743a31a0' target='blank'><BsLinkedin/></a>
       <a href='https://github.com/NashHub1' target='blank'><BsGithub/></a>
-
     </div>
   )
 }
